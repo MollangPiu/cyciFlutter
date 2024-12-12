8cb2aeadc83c5b5273f0a3d8db09076e3bdb84d3
MHS <hayaruby7909@gmail.com>
2024-12-12 11:33:17 +0900

    first commit

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'Home.dart';
+import 'About.dart';
+import 'def/Def.dart';
+
+
+//Java Map ( HashMap Key, Value )
+final Map<String, WidgetBuilder> routes = {
+  '/home':  (context) => Home(),
+  '/about': (context) => About(),
+  '/def': (context) => Def(),
+};
